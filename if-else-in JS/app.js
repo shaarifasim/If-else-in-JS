746b158636716268f4beb23fe0926c6757e94e00
Shaarif Asim <84714602+shaarifasim@users.noreply.github.com>
2022-05-31 17:46:55 +0500

    Add files via upload

---
+++ if-else-in JS/app.js
@@ -0,0 +1,117 @@
+// var  my_time = new Date();
+// alert(my_time);
+
+// var greet;
+// var input=prompt("Enter Time In 24 hrs format( 19:00=7pm)");
+// if(input>=0000 && input<1200){
+//     alert("Good Morning!");
+// }
+// else if(input>=1200 && input<1700){
+//     alert("Good afternoon");
+// }
+// else if(input>=1700 && input<2100){
+//     alert("Good Evening");
+// }
+// else{
+//     alert("Good Night");
+// }
+
+    
+ //leap year program
+		 
+// var year = prompt("Enter Year to check if its leap year or not:(ie:2005)");
+ 
+		
+//     if( (0 == year % 4) && (0 != year % 100) || (0 == year % 400) )
+// 	{
+// 		alert(year+" is a leap year");  
+// 	}
+// 	else
+// 	{
+// 		alert(year+" is not a leap year");  
+// 	}
+
+// password check program
+// var create=prompt("Create you password without space:");
+// var real=prompt("Now enter your password that you created in order to proceed");
+
+// if(real===""){
+//     alert("Plz Enter Password");
+// }
+// else if(real!=create){
+//     alert("Password does not match");
+// }
+// else{
+// if(real===create){
+//     alert("Success!");
+// }}
+
+//11
+// var firstName = "shaarif";
+// if (firstName === "Asim") {
+//  document.write("Hello shary!");
+// }
+// else{
+//     document.write("You are not Sary!")
+// }
+
+//12
+// var greeting;
+//  var hour = 13;
+//  if (hour < 18) {
+//  greeting = "Good day";
+//  alert(greeting);
+//  }
+//  else{
+//  greeting = "Good evening";
+//  alert(greeting);
+//  } 
+
+ //13
+//  var num1=prompt("Enter num 1");
+//  var num2=prompt("Enter num 2");
+
+//  if(num1<num2){
+//      alert("number 1 is smaller than number 2");
+//  }
+// else if(num1>num2){
+//     alert("number 1 is greater than number 2");
+
+// }
+// else{
+//     if (num1===num2) {
+//         alert("Both are same numbers!");
+//     }
+// }
+
+//16
+// var variable ;
+// variable = prompt("Enter any character or word.");
+// if (variable >= "0") {
+//   alert("It is a number");
+// }
+// else if (variable === "true" || variable === "false" || variable === "yes" || variable === "no") {
+//   alert("It is a boolean");
+// }
+// else if (variable === "") {
+//   alert("It is a string");
+// }
+// else {
+//   alert("It is an undefined character or action");
+// }
+
+//17
+// var vowel ;
+// vowel = prompt("Enter an alphabet");
+// if (vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u") {
+//   alert("It is a vowel");
+// }
+// else {
+//   alert("It is not a vowel");
+// }
+
+//18
+// var trueNo ;
+// if ("10" !== "8") {
+//   alert("true");
+// }
